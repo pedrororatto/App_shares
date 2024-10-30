f5954b9415174b1b83f3d695d6694182fae52421
pedro.roratto <pedro_roratto@hotmail.com>
2024-10-30 20:46:06 -0300

    add app to deploy

---
+++ main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import seaborn as sns
+from sklearn import datasets
+import streamlit as st
+
+df = sns.load_dataset('iris')
+
+st.title('Análise de COnjunto de Dados Iris')
+st.write(df.head())
+
+st.subheader('Estatisitcas Descritivas')
+st.write(df.describe())
+
+#Graficos intereativos
+st.subheader('Gráfico de Dispersão: Sepal Lenght vc Sepal Width')
+st.write('Visualização de dispersão entre as variáveis Sepal Length e Sepal Width')
+scatter_plot = sns.scatterplot(x='sepal_length', y='sepal_width', hue='species', data=df)
+st.pyplot(scatter_plot.figure)
+
+st.subheader('Distribuição do comprimento da pétala')
+st.write('Distribuição do comprimento da pétala por espécie')
+hist_plot = sns.histplot(data=df, x='petal_length', hue='species', kde=True)
+st.pyplot(hist_plot.figure)
